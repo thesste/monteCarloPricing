9b37186af2ecd46fe12a831ea9f4b7ae9c008a06
chris <chris@chris-T420>
2020-09-16 09:39:37 +0200

    Included Test Against Analytical Solution

---
+++ monteCarloPricing/src/test/java/monteCarloPricing/CalculateOptionPriceTest.java
@@ -0,0 +1,36 @@
+package monteCarloPricing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import net.finmath.functions.AnalyticFormulas;
+
+public class CalculateOptionPriceTest {
+
+	@Test
+	public void testCalculateOptionPrice() {
+		
+		int numberOfPaths = 1000000;
+		double maxMaturity = 1;
+		double underlyingPrice = 100;
+		double volatility = 0.2;
+		double riskFreeRate = 0.05;
+		int numberOfTradingDaysPerYear = 1;
+		
+		MCPricer pricer = new MCPricer(underlyingPrice, volatility, riskFreeRate, numberOfPaths, numberOfTradingDaysPerYear, maxMaturity);
+		pricer.generatePaths();
+		
+		double strike = 90;
+		double barrier = Double.NaN;
+		double maturity = 1;
+		String optionType = "Call";
+		String barrierType = "None";
+		
+		double price = pricer.calculateOptionPrice(strike, barrier, maturity, optionType, barrierType);
+
+		double benchmark = AnalyticFormulas.blackScholesOptionValue(underlyingPrice, riskFreeRate, volatility, maturity, strike);		
+		double tolerance = 0.1;		
+		assertEquals(benchmark, price, tolerance);		
+	}
+
+}
